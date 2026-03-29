a3ed43014656b4b1b898aee637447dee47264e89
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-29 20:40:20 -0300

    Acrescentando a rota de gerar memorandos na principal

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { memoRoutes } from './memo-route.js'
+import { pdfRoutes } from './pdf-route.js'
 
 const routes = Router()
 
 routes.use("/memorandos", memoRoutes)
+routes.use("/generate", pdfRoutes)
 
 export { routes }
