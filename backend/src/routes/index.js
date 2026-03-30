fac49cea6a66f5f3e69daa8108ead8c47851dfdc
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-30 17:05:38 -0300

    adicionando a rota de /sendmail

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { memoRoutes } from './memo-route.js'
 import { pdfRoutes } from './pdf-route.js'
+import { mailRoutes } from './mail-route.js'
 
 const routes = Router()
 
 routes.use("/memorandos", memoRoutes)
 routes.use("/generate", pdfRoutes)
+routes.use("/sendmail", mailRoutes)
 
 export { routes }
